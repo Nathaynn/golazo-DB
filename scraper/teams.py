601961b0135877dd4116f7fe947b18e4e5783de7
Nathan Martinez <nmart110@calpoly.edu>
2024-02-26 21:30:35 -0800

    Update teams.py

--- scraper/teams.py
+++ scraper/teams.py
@@ -11,17 +11,24 @@ url = "https://fbref.com/en/"
 driver = webdriver.Chrome()
 driver.get(url)      
 
+# get leagues + their corresponding team
 drop_league_button = Select(driver.find_element(By.NAME, 'league_val'))
 
 arr_of_teams = []
+
 for i in leagues:
-    x = {}
     drop_league_button.select_by_visible_text(i["league_name"])
 
     drop_club_button = driver.find_element(By.ID, 'team_choice')
 
     clubs = [x.get_attribute('innerHTML') for x in drop_club_button.find_elements(By.TAG_NAME, 'option')]
     clubs.pop(0)
-    print(clubs)
+    for j in clubs:
+        x = {'league_name': i}
+        x['team_name'] = j
+        arr_of_teams.append(x)
+
+# arr_of_teams = [{league_name: name, team_name: name_2}, ...]
+# needs city, stadium, and year of founding
 
 sleep(10)
