d96be6e784427be501d57cdd2f79f75d00e183f5
Nathan Martinez <nmart110@calpoly.edu>
2024-03-08 03:07:14 -0800

    Update players.py
    Player scraping completed. need to do managers once relationship is solidified.

--- scraper/players.py
+++ scraper/players.py
@@ -6,7 +6,6 @@ from teams import open_url, get_season_link
 import json
 
 # get data for every team within a league + season
-# also note that this grabs the managers stuff too
 
 def league_player_data(url, driver):
     open_url(url, driver)
@@ -21,7 +20,7 @@ def league_player_data(url, driver):
         href = href_area.get_attribute('href')
 
         # open tab and check out the team stats
-        driver.execute_script('window.open(https://www.google.com)')
+        driver.execute_script("window.open('https://www.google.com')")
         driver.switch_to.window(driver.window_handles[1])
         driver.get(href)
 
@@ -37,12 +36,12 @@ def league_player_data(url, driver):
                 staff = g.find_elements(By.CLASS_NAME, 'staff')
                 # quadruple loop :O
                 for y in staff:
-                    player = y.find_element(By.TAG_NAME, 'a')
+                    player = y.find_element(By.CLASS_NAME, 'text').find_element(By.TAG_NAME, 'a')
                     player_link = player.get_attribute('href')
 
                     # player history
                     hist = {}
-                    hist['Player Name'] = y.find_element(By.TAG_NAME, 'a').get_attribute('innerHTML')
+                    hist['Player Name'] = y.find_element(By.CLASS_NAME, 'text').find_element(By.TAG_NAME, 'a').get_attribute('innerHTML')
                     hist['Team Name'] = team_id
                     hist['Player Number'] = y.find_element(By.CLASS_NAME, 'number').get_attribute('innerHTML')
                     hist['Season Year'] = (Select(driver.find_element(By.ID, 'page').find_element(By.TAG_NAME, 'form').find_element(By.TAG_NAME, 'select'))).first_selected_option.get_attribute('innerHTML')[0:4]
@@ -51,23 +50,66 @@ def league_player_data(url, driver):
                     player_stuff = player_data(player_link, driver)                    
                     player_stuff['Player Position'] = position
                     players.append(player_stuff)
+                    # debugging
+                    print(player_stuff)
+                    print(hist)
+
         driver.close()
         driver.switch_to.window(driver.window_handles[0])
+
 
 # data for a SINGLE player
 def player_data(url, driver):
     # store current tab and go to player tab
-    driver.execute_script('window.open(https://www.google.com)')
+    driver.execute_script("window.open('https://www.google.com')")
     current_window = driver.current_window_handle
     player_window = len(driver.window_handles) - 1
     driver.switch_to.window(driver.window_handles[player_window])
     driver.get(url)
 
-    # everything else goes here
-    
-
-
-    # close tab and go back to original tab
+    # each div within bio can't be uniquely identified without looking inside inside each element
+    # check each div. if it has the stuff we want then commit, else ignore
+    important_stuff = ['Name', 'Height', 'Weight', 'Nationality', 'Born/Age']
+    bio = driver.find_element(By.ID, 'entity_bio')
+    bios = bio.find_elements(By.CLASS_NAME, 'bio')
+    bios += bio.find_elements(By.CLASS_NAME, 'bio_half')
+    player_fname = ''
+    player_lname = ''
+    player_age = 0
+    player_height = 0
+    player_weight = 0
+    player_nationality = ''
+    for i in bios:
+        try:
+            span = i.find_element(By.TAG_NAME, 'span').get_attribute('innerHTML')
+        except:
+            continue
+        if span in important_stuff:
+            if span == 'Name':
+                temp_name = i.get_attribute('innerHTML').replace('<span>Name</span>', "")
+                temp_name = temp_name.split()
+                player_fname = temp_name[0]
+                player_lname = temp_name[-1]
+            elif span == 'Height':
+                temp_height = i.get_attribute('innerHTML').replace('<span>Height</span>', "")
+                temp_height = temp_height.split()
+                player_height = int(temp_height[0])
+            elif span == 'Weight':
+                temp_weight = i.get_attribute('innerHTML').replace('<span>Weight</span>', "")
+                temp_weight = temp_weight.split()
+                player_weight = int(temp_weight[0])
+            elif span == 'Nationality':
+                player_nationality = i.find_element(By.CLASS_NAME, 'text').get_attribute('innerHTML')
+            elif span == 'Born/Age':
+                temp_age = i.find_element(By.CLASS_NAME, 'small').get_attribute('innerHTML')[1:]
+                temp_age = temp_age.split()
+                player_age = int(temp_age[0])
+    # close and return
     driver.close()
-    driver.switch_to.window(driver.current_window)
+    driver.switch_to.window(current_window)
+    return {'Player Fname': player_fname, 'Player Lname': player_lname, 'Player Age': player_age, 'Player Height': player_height, 'Player Weight': player_weight, 'Player Nationality': player_nationality}
+
+
+driver = webdriver.Chrome()
+x = league_player_data('https://www.playmakerstats.com/edition/premier-league-2022-2023/165592', driver)
 
